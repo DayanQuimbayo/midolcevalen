b2b035445244123e12746a51b4b246beadf9825b
DayanQuimbayo <dayanquimbayo@gmail.com>
2025-11-27 12:41:43 -0500

    Elimino todos los archivos del repositorio sin borrar local

--- backend/read.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../database.php';
-
-$query = $pdo->query("SELECT * FROM usuarios");
-$usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($usuarios);
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'conexion.php';
-echo "✔ Conexión exitosa con la base de datos mi_dolce_valen";
-?>
